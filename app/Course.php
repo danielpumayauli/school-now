d91b32d8769dbdc05fbfe49d7e610a90411d36ee
danielpumayauli <danielpumayauli@gmail.com>
2018-05-02 05:11:24 -0500

    Agregadas relaciones entre clases de cursos y temas.

--- app/Course.php
+++ app/Course.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Subject;
 
 class Course extends Model
 {
-    //
+    // 1 curso tiene muchos temas
+
+    public function subjects()
+    {
+    	return $this->hasMany(Subject::class);
+    }
 }
  
--- app/Subject.php
+++ app/Subject.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Course;
 
 class Subject extends Model
 {
-    //
+    // 1 tema pertenece a 1 curso
+
+    public function course()
+    {
+    	return $this->belongsTo(Course::class);
+    }
 }
